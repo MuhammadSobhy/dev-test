1384f5983d9b4b446828f7c5ce5ccae3a50881c9
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-08-30 11:19:52 +0200

    Add city and geo location entities to the project.

---
+++ GoEuroTest/src/main/java/com/goeuro/entity/City.java
@@ -0,0 +1,10 @@
+package com.goeuro.entity;
+
+/**
+ * City obtained from JSON API
+ * @author muhammad sobhy
+ * @version 0.0
+ */
+public class City {
+    
+}
---
+++ GoEuroTest/src/main/java/com/goeuro/entity/GeoLocation.java
@@ -0,0 +1,10 @@
+package com.goeuro.entity;
+
+/**
+ * Contains city's location information
+ * @author muhammad sobhy
+ * @version 0.0
+ */
+public class GeoLocation {
+    
+}

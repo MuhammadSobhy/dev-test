972a00321d39a84a9120b9f575d78498e55e6d6a
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-09-25 14:02:37 +0200

    Add city properties and jackson annotations

--- GoEuroTest/src/main/java/com/goeuro/entity/City.java
+++ GoEuroTest/src/main/java/com/goeuro/entity/City.java
@@ -1,10 +1,174 @@
 package com.goeuro.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * City obtained from JSON API
  * @author muhammad sobhy
- * @version 0.0
+ * @version 1.0
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class City {
     
+    public enum Header{
+        
+        ID("_id"),
+        NAME("name"),
+        TYPE("type"),
+        LATITUDE("latitude"),
+        LONGITUDE("longitude");
+        
+        String headerName;
+        
+        private Header(String headerName) {
+            this.headerName = headerName; 
+        }
+        
+        public String getName() {
+            return this.headerName;
+        }
+        
+    }
+    
+    @JsonProperty("_id")
+    private long id;
+    @JsonProperty("key")
+    private String key;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("fullName")
+    private String fullName;
+    @JsonProperty("iata_airport_code")
+    private String iataAirportCode;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("country")
+    private String country;
+    @JsonProperty("geo_position")
+    private GeoPosition geoPosition;
+    @JsonProperty("locationId")
+    private long locationID;
+    @JsonProperty("inEurope")
+    private boolean inEurope;
+    @JsonProperty("countryId")
+    private String countryId;
+    @JsonProperty("countryCode")
+    private String countryCode;
+    @JsonProperty("coreCountry")
+    private boolean coreCountry;
+    @JsonProperty("distance")
+    private double distance;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getIataAirportCode() {
+        return iataAirportCode;
+    }
+
+    public void setIataAirportCode(String iataAirportCode) {
+        this.iataAirportCode = iataAirportCode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public GeoPosition getGeoPosition() {
+        return geoPosition;
+    }
+
+    public void setGeoPosition(GeoPosition geoPosition) {
+        this.geoPosition = geoPosition;
+    }
+
+    public long getLocationID() {
+        return locationID;
+    }
+
+    public void setLocationID(long locationID) {
+        this.locationID = locationID;
+    }
+
+    public boolean isInEurope() {
+        return inEurope;
+    }
+
+    public void setInEurope(boolean inEurope) {
+        this.inEurope = inEurope;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public boolean isCoreCountry() {
+        return coreCountry;
+    }
+
+    public void setCoreCountry(boolean coreCountry) {
+        this.coreCountry = coreCountry;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }    
 }

3c3665cde0c3594b3581179af94f4410d3f54ad2
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-09-25 15:53:33 +0200

    Implement method to inquire cities from web service

--- GoEuroTest/src/main/java/com/goeuro/service/RestCityBusinessService.java
+++ GoEuroTest/src/main/java/com/goeuro/service/RestCityBusinessService.java
@@ -1,6 +1,12 @@
 package com.goeuro.service;
 
 import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 /**
  * Rest Business service to call REST web service and return city
@@ -16,6 +22,26 @@ public class RestCityBusinessService extends CityBusinessService{
     
     @Override
     public String getCityByName(String cityName) throws IOException{
-        return null;
+        String jsonResponse = "";
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet(getUrl() + cityName);
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            jsonResponse = EntityUtils.toString(entity);
+        } catch (IOException ex) {
+            System.out.println("calling city service failed: " + ex);
+            throw ex;
+        } finally {
+            try {
+                if(response != null) 
+                    response.close();
+            } catch (IOException ex) {
+                System.out.println("closing service response failed: " + ex);
+                throw ex;
+            }
+        }
+        return jsonResponse;
     }
 }

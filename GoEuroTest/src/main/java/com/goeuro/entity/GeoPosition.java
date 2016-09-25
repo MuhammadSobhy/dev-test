4fae8037a70f52fee9544ff71c9b98cc850e270d
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-09-25 14:06:37 +0200

    Add city's geo position properties and jackson annotations

--- GoEuroTest/src/main/java/com/goeuro/entity/GeoPosition.java
+++ GoEuroTest/src/main/java/com/goeuro/entity/GeoPosition.java
@@ -1,10 +1,32 @@
 package com.goeuro.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
- * Contains city's location information
+ * Contains city's position information
  * @author muhammad sobhy
- * @version 0.0
+ * @version 1.0
  */
-public class GeoLocation {
+public class GeoPosition {
     
+    @JsonProperty("latitude")
+    private double latitude;
+    @JsonProperty("longitude")
+    private double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }    
 }

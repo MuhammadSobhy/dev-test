f4054074a80d5a786ff960eaa7da85d855d40b93
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-09-25 16:08:22 +0200

    Implement CSV support methods

--- GoEuroTest/src/main/java/com/goeuro/output/CSVExporter.java
+++ GoEuroTest/src/main/java/com/goeuro/output/CSVExporter.java
@@ -2,6 +2,10 @@ package com.goeuro.output;
 
 import com.goeuro.entity.City;
 import java.util.List;
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -9,9 +13,22 @@ import java.util.List;
  */
 public class CSVExporter {
     
-    public void export(String fileName , String[] headers , List<City> cityData){
-        
+    public void export(String fileName , String[] headers , List<City> cityData) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
+            writer.writeNext(headers);
+            writer.writeAll(toListOfStringArray(cityData));
+        }
     }
     
-    
+    private List<String[]> toListOfStringArray(List<City> cities) {
+        List<String[]> stringArrayOfCities = null;
+        if (cities != null && cities.size() > 0) {
+            stringArrayOfCities = new ArrayList<>();
+            for (City city : cities) {
+                stringArrayOfCities.add(new String[] {String.valueOf(city.getId()),city.getName(),city.getType(),
+                    String.valueOf(city.getGeoPosition().getLatitude()),String.valueOf(city.getGeoPosition().getLongitude())});
+            }
+        }
+        return stringArrayOfCities;
+    }
 }

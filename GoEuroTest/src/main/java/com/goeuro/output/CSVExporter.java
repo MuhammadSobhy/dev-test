3b6319a41f50a0f4b9cf02ca4d3cb5ac03d0eeda
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-09-25 16:06:17 +0200

    Add csv exporter class to support csv files mechanim

---
+++ GoEuroTest/src/main/java/com/goeuro/output/CSVExporter.java
@@ -0,0 +1,17 @@
+package com.goeuro.output;
+
+import com.goeuro.entity.City;
+import java.util.List;
+
+/**
+ *
+ * @author muhammad sobhy
+ */
+public class CSVExporter {
+    
+    public void export(String fileName , String[] headers , List<City> cityData){
+        
+    }
+    
+    
+}

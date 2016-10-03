4206c5c47e6c8addfbd03c9db6cde997c9422d2b
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-10-03 23:22:43 +0200

    Add logging

--- GoEuroTest/src/main/java/com/goeuro/service/RestCityBusinessService.java
+++ GoEuroTest/src/main/java/com/goeuro/service/RestCityBusinessService.java
@@ -8,6 +8,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Rest city business service
@@ -16,6 +18,7 @@ import org.apache.http.util.EntityUtils;
  */
 public class RestCityBusinessService extends CityBusinessService{
 
+    private static final Logger LOGGER = LogManager.getRootLogger();
     private final String url;
     
     public RestCityBusinessService(String url) {
@@ -23,37 +26,40 @@ public class RestCityBusinessService extends CityBusinessService{
     }
     
     /**
-     * Call REST web service to inquire about specific city
+     * Call REST web service to inquire specific city
      * @param cityName
      * @return jsonResponse contains city info
      * @throws NoSuchCity 
      */
     @Override
     public String getCityByName(String cityName) throws NoSuchCity{
+        LOGGER.debug("Entering getCityByName(cityName="+ cityName +")");
         String jsonResponse = "";
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpget = new HttpGet(getUrl() + cityName);
         CloseableHttpResponse response = null;
+        LOGGER.debug("City REST web service url:" + getUrl());
         try {
             response = httpClient.execute(httpget);
             HttpEntity entity = response.getEntity();
             jsonResponse = EntityUtils.toString(entity);
         } catch (IOException ex) {
-            throw new NoSuchCity("calling city REST service failed",ex);
+            throw new NoSuchCity("Calling city REST service failed",ex);
         } finally {
             try {
                 if(response != null) 
                     response.close();
             } catch (IOException ex) {
-                System.out.println("closing service response failed: " + ex);
+                LOGGER.error("Closing city REST response failed",ex);
             }
             try {
                 if(httpClient != null) 
                     httpClient.close();
             } catch (IOException ex) {
-                System.out.println("closing service response failed: " + ex);
+                LOGGER.error("Closing city REST client failed",ex);
             }
         }
+        LOGGER.debug("Leaving getCityByName():" + jsonResponse);
         return jsonResponse;
     }
 

f9fb5f5de2df302dd6857a2f4d601fb328e28410
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-10-03 21:26:32 +0200

    1- Remove url property. 2- Make getCityByName method more generic.

--- GoEuroTest/src/main/java/com/goeuro/service/CityBusinessService.java
+++ GoEuroTest/src/main/java/com/goeuro/service/CityBusinessService.java
@@ -1,35 +1,15 @@
 package com.goeuro.service;
 
-import java.io.IOException;
+import com.goeuro.exception.NoSuchCity;
 
 
 /**
- * city business service to be implemented by different city storage mechanism (JSON, XML, ...)
+ * City business service to be implemented by different city storage mechanisms (JSON, XML,DB ...)
  * @author muhammad sobhy
  * @version 1.0
  */
 public abstract class CityBusinessService implements BusinessService{
 
-    private String url;
+    public abstract <T extends Object> T getCityByName(String cityName) throws NoSuchCity;
 
-    public CityBusinessService(String url) {
-        this.url = url;
-    }   
-    
-    @Override
-    public String getBusinessServiceName() {
-        return "City Business Service";
-    }
-    
-    public abstract String getCityByName(String cityName) throws IOException;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-    
-    
 }

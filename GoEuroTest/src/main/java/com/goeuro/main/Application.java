759c8aa39300f50c8e75bf9833ffec339cc95272
Mohamed Sobhy <mahmad@eg.ibm.com>
2016-10-04 02:05:09 +0200

    Add logging

--- GoEuroTest/src/main/java/com/goeuro/main/Application.java
+++ GoEuroTest/src/main/java/com/goeuro/main/Application.java
@@ -6,6 +6,8 @@ import com.goeuro.exception.NoSuchConfigurationKey;
 import com.goeuro.facade.CityFacade;
 import java.io.IOException;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 /**
@@ -15,24 +17,28 @@ import java.util.List;
  */
 public class Application {
 
-      
+    private static final Logger LOGGER = LogManager.getRootLogger();      
     CityFacade mainFacade;
     
     public static void main(String[] args){
+        LOGGER.debug("Entering main(args=" + args + ")");
         if (args == null || args.length <= 0) {
-            System.out.println("Please pass city name to app.");
+            LOGGER.debug("Please pass city name to app.");
         } else {
             try {
                 new Application().run(args[0]);
             } catch (IOException | NoSuchCity | NoSuchConfigurationKey ex) {
-                System.out.println(ex);
+                LOGGER.error(ex.getMessage() , ex);
             }
         }
+        LOGGER.debug("Leaving main()");
     }
     
     private void run(String cityName) throws NoSuchConfigurationKey, NoSuchCity, IOException {
+        LOGGER.debug("Entering run(cityName=" + cityName + ")");
         mainFacade = new CityFacade();
         List<City> cities = mainFacade.retrieveCityFromRestService(cityName);
         mainFacade.exportToCSV(cities);
+        LOGGER.debug("Leaving run()");
     }    
 }
